c4adbf7b74f00c701ad31a80685ca30e9808073c
bhuvankumarsp03 <153191438+bhuvankumarsp03@users.noreply.github.com>
2023-12-07 17:40:59 +0530

    Add files via upload

---
+++ class1/Microservices3/DepartmentService/Controllers/DepartmentController.cs
@@ -0,0 +1,41 @@
+﻿using DepartmentService.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DepartmentService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentController : ControllerBase
+    {
+        private readonly IDepartmentRepository _repository;
+
+        public DepartmentController(IDepartmentRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var data = _repository.GetAllDepartments();
+            return Ok(data);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var data = _repository.GetDepartmentById(id);
+            if(data == null) 
+                return NotFound("no record found");
+            return Ok(data);
+        }
+
+        [HttpPost]
+        public IActionResult Post(Department department)
+        {
+            var data = _repository.AddDepartment(department);
+            return Ok(data);
+        }
+    }
+}
---
+++ class1/Microservices3/DepartmentService/Models/Department.cs
@@ -0,0 +1,9 @@
+﻿namespace DepartmentService.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+    }
+}
